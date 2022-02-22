8faac270327b811fcd8aea520275ee7f87d32edd
Krzysztof Jablonski <kj@something.com>
2022-02-22 23:20:39 +0100

    a bit more documentation and code examples.

--- doc/Code.Example.Functional.Extensions/TryParseUsages.cs
+++ doc/Code.Example.Functional.Extensions/TryParseUsages.cs
@@ -1,5 +1,7 @@
-﻿using Kj.Functional.Lib.Core;
+﻿using System.Diagnostics.Contracts;
+using Kj.Functional.Lib.Core;
 using Kj.Functional.Lib.Extensions.Parse;
+using Option;
 
 namespace Code.Example.Functional.Extensions;
 
@@ -21,4 +23,35 @@ public static class TryParseUsages
 			.Or(() => $"Invalid date") // map to another string if anything went wrong
 			;
 	}
+
+	public record UserInfo(string UserName, int YearOfBirth, double NumberOfPoints);
+
+	private static Option<string> TryParseName(this string input)
+	{
+		if (input.Length > 5)
+		{
+			return input;
+		}
+
+		return Of.None;
+	}
+	
+	public static Option<UserInfo> TryParseUserFromStrings(string inputName, string birthYear, string numberPoints)
+	{
+		return _createUserFunc(
+			inputName.TryParseName(), 
+			birthYear.TryParseInt(), 
+			numberPoints.TryParseDouble()
+			);
+	}
+	
+	// Optional values are "passed further" in the chained call to finally create an object from all "extracted values" (if present)
+	// Any value being empty breaks the chain, leading to a None value being returned instead of the parsed object
+	private static readonly Func<Option<string>, Option<int>, Option<double>, Option<UserInfo>> _createUserFunc =
+		(optUserName, optYearOfBirth, optUserPoints) =>
+			optUserName.Bind(name => optYearOfBirth.Bind(year => optUserPoints.Map(
+						p => new UserInfo(name, year, p)
+					)
+			));
+
 }
