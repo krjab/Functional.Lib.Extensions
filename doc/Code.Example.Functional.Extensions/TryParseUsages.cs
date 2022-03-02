f0f7cf1f17fb2961b5cb95707198f677cb27d146
Krzysztof Jablonski <kj@something.com>
2022-03-02 21:46:41 +0100

    cleanup and changed version to 0.1.3

--- doc/Code.Example.Functional.Extensions/TryParseUsages.cs
+++ doc/Code.Example.Functional.Extensions/TryParseUsages.cs
@@ -1,8 +1,6 @@
-﻿using System.Diagnostics.Contracts;
-using Kj.Functional.Lib.Core;
+﻿using Kj.Functional.Lib.Core;
 using Kj.Functional.Lib.Extensions.Models.Validation;
 using Kj.Functional.Lib.Extensions.Parse;
-using Option;
 
 namespace Code.Example.Functional.Extensions;
 
@@ -20,6 +18,7 @@ public static class TryParseUsages
 	public static void ParseDateValidateAndMap(string inputString)
 	{
 		var result = inputString.TryParseDateTime() // try parse value
+			.AsOption()
 			.Filter(d => d.Year > 1999) // validate if year > 1999
 			.Map(d => $"valid input date of {d}") // map valid date to output string
 			.Or(() => $"Invalid date") // map to another string if anything went wrong
